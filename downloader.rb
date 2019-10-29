1fe091606fd50f678ffa30b8b4c7839148dfd1c0
Georgi Berchev <g.berchev@gmail.com>
2019-10-29 15:43:43 +0200

    add final script

--- downloader.rb
+++ downloader.rb
@@ -1,16 +1,68 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'rbconfig'
+require 'open-uri'
+require 'zip'
+require 'dir'
+require 'fileutils'
 
-uri = URI.parse("https://releases.hashicorp.com/terraform/index.json")
+################# Modules deffinition ####################
+# Determine host OS
+module OperatingSystem
+  def self.name
+    case RbConfig::CONFIG['host_os']
+
+    when /linux/
+      'linux'
+    when /darwin/
+      'darwin'
+    else
+      "Unknown OS for this script #{RbConfig::CONFIG['host_os']}"
+    end
+  end
+end
+
+# Determine host Architecture
+module Architecture
+  def self.name
+    case RbConfig::CONFIG['host_cpu']
+
+    when /amd64|x86_64/
+      'amd64'
+    when /i?86|x86|i86pc/
+      '386'
+    when /^arm/
+      'arm'
+    else
+      "Unknown Architecture for this script #{RbConfig::CONFIG['host_cpu']}"
+    end
+  end
+end
+
+############# Variables definition ###########################
+# Product you want to download
+product = "packer"
+
+# Directory where the product will be downloaded
+tools_directory = "#{Dir.home}/hashicorp_tools"
+
+# Link to all hashicorp products in JSON format
+hashicorp_products_url = "https://releases.hashicorp.com/#{product}/index.json"
+
+bash_profile_path = "#{Dir.home}/.bash_profile"
+path_configuration = 'export PATH="$HOME/hashicorp_tools:$PATH"'
+############## Starting main program #########################
+# Start the process of filtering versions
+uri = URI.parse(hashicorp_products_url)
 response = Net::HTTP.get_response(uri)
 
 # body method will extract the whole HTTP output
 versions = JSON.parse(response.body)['versions']
 all_versions = JSON.parse(response.body)['versions'].keys
-#target_url = "https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_darwin_amd64.zip"
 
-# Resulted array of strings in format "x.y.z" with stabele versions only!
+# Filter the stable versions only!
+# Resulted array of strings in format "x.y.z" 
 stable_versions = []
 all_versions.each do |element|
   unless element.match("[a-zA-Z]")
@@ -18,7 +70,7 @@ all_versions.each do |element|
   end
 end
 
-p stable_versions
+stable_versions
 
 # Find latest version by comparing stable_versions elements
 max = Gem::Version.new('0.0.0')
@@ -32,20 +84,36 @@ end
 
 latest_version = max.to_s
 
-p latest_version
-# Convert stable_versions array into nested array in order to find highest version. 
-#nested_versions = []
-#stable_versions.each do |string|
-#  nested_versions.push(string.split("."))
-#end
-
-#p nested_versions
-
-# Find the latest version
-# from latest version, find OS architecture
 # Array of hashes => [{}, {}, {}]
 # {"name"=>"terraform", "version"=>"0.12.10", "os"=>"darwin", "arch"=>"amd64", "filename"=>"terraform_0.12.10_darwin_amd64.zip", "url"=>"https://releases.hashicorp.com/terraform/0.12.10/terraform_0.12.10_darwin_amd64.zip"}
+# Iterate through the array of hashes and match your OS type and Architecture, then find the proper download link
+target_url = ""
+products_by_os = versions[latest_version]['builds']
+products_by_os.each do |hash|
+  if hash.has_value?(OperatingSystem.name) && hash.has_value?(Architecture.name)
+    target_url = hash.fetch("url")
+  end
+end
 
-type = versions[latest_version]['builds'][0]
-p type
+# Create download directory if such do not exists
+FileUtils.mkdir_p(tools_directory, :mode => 0755) unless Dir.exists?(tools_directory)
 
+# Get download link content
+content = open(target_url)
+
+# Download and extract Hashicorp product into tools_directory (product will be overwritten, if already exists)
+# and add executable permissions
+Zip::File.open_buffer(content) do |zip|
+  zip.each do |file|
+    file.extract("#{tools_directory}/#{file.name}") { true }
+    FileUtils.chmod("+x", "#{tools_directory}/#{file.name}")
+  end
+end
+
+# Check whether hashicorp_tools exists into your $PATH and add it, if needed
+if !File.read(bash_profile_path).include?(path_configuration)
+  File.open(bash_profile_path, 'a') do |file| 
+    file.puts
+    file.write(path_configuration)
+  end
+end
